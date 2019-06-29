2ab50bb9270e686176deff097bc61e5d532c9988
slumfrog <kishan.anil@gmail.com>
2019-06-29 21:01:15 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+
+attr_accessor :name
+
+    def initialize
+        @name = name  
+    end
+
+    def name
+        @name
+    end
+
+end
+
+
+
+# binding.pry
+# 0
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Song
+
+attr_accessor :title, :artist
+
+    def initialize
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+end
+
+# binding.pry
+# 0
